beeb33aaa0efb915e6a5f098d3eb5def2256d112
William Gilley <wsg9@txstate.edu>
2016-02-03 04:18:26 -0600

    initial commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "<H1>Hello World!</H1>";
+        //}
+
+        
+        public ViewResult Index()
+        {
+            // return the index.cshtml (page or view) under the
+            // Views/Home
+            return View("Index");
+        }
+
+        // http://localhost:55674/Home/RsvpForm
+        [HttpGet]  // to 
+
+        public ViewResult RsvpForm()
+        {
+            return View("RsvpForm");
+        }
+
+        [HttpPost]  // to receive form data, POST
+
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            return View("Thanks");
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        //define the properties
+        public string UserName { set; get; }
+    }
+}
